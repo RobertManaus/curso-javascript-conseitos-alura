ab1b0a47d602024cf0c598a73e5e89ff73f3a499
RobertManaus <robeertjr@gmail.com>
2022-06-07 18:57:26 -0400

    exemplos da aula 1

---
+++ type-number.js
@@ -0,0 +1,26 @@
+// Tipo Number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero= 2;
+
+const operacaoMatematica = primeiroNumero * segundoNumero;
+
+console.log(operacaoMatematica);
+
+// Ponto flutuante
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+
+console.log(novaOperacao);
+
+// NoN -> Not A Number (Não é um número)
+
+const alura = "Alura";
+console.log(alura*primeiroNumero);
+
+
---
+++ type-string.js
@@ -0,0 +1,15 @@
+
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura456!";
+const StringdeNumeros = "34567";
+
+const citacao = "Meu nome é ";
+const meuNome = "Leonardo";
+
+// Concatenação ( + )
+
+console.log(citacao + meuNome);
+
+// Templete string OU templete literal
+
